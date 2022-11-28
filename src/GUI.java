9f97a72b9d8c8e516a25bb6d4fad3f61cd0e44a1
Tymoteusz <tymoteusz.lango@gmail.com>
2022-11-28 16:22:52 +0100

    Update GUI

--- src/GUI.java
+++ src/GUI.java
@@ -6,16 +6,18 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.*;
 
 public class GUI implements ActionListener{
-    JFrame frame;
-    JPanel buttonPanel, functionPanel, container;
-    JButton[][] buttons;
-    int size_x, size_y, liczba_min;
-    boolean start = true;
-    int[][] board_of_numbers;
-    boolean[][] board_of_mines;
+    private JFrame frame;
+    private JPanel buttonPanel, functionPanel, container;
+    private JButton[][] buttons;
+    private int size_x, size_y, liczba_min;
+    private boolean start = true;
+    private int[][] board_of_numbers;
+    private boolean[][] board_of_mines;
+    private boolean isFlagging = false;
 
     public GUI(int size_x, int size_y, int liczba_min){
         this.size_x = size_x;
@@ -26,7 +28,6 @@ public class GUI implements ActionListener{
         startgui();
     }
 
-    /**GUI */
     private void startgui(){
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -38,7 +39,7 @@ public class GUI implements ActionListener{
 
         functionPanel = new JPanel();
         functionPanel.setVisible(true);
-        functionPanel.setLayout(new GridLayout(0, 3));
+        functionPanel.setLayout(new GridLayout(0, 4));
 
         //Ilość zaznaczonych min
         JLabel mines = new JLabel(Integer.toString(liczba_min));
@@ -48,6 +49,11 @@ public class GUI implements ActionListener{
         //RESET button
         JButton resetBT = new JButton("RESET");
         functionPanel.add(resetBT);
+
+        //FLAGA button
+        JButton flagaBT = new JButton("🚩");
+        flagaBT.addActionListener(this::flagPerformed);
+        functionPanel.add(flagaBT);
 
         //Licznik czasu
         JLabel time = new JLabel("0");
@@ -76,33 +82,71 @@ public class GUI implements ActionListener{
         frame.repaint();
     }
 
-    /**GUI */
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        for (int i = 0; i < size_x; i++) {
-            for (int j = 0; j < size_y; j++) {
-                if (event.getSource() == buttons[i][j]){
-                    if (start){
-                        GameLogic.randomBoardOfMines();
-                        // Nie działa :C
-                        while (board_of_mines[i][j]){
-                            GameLogic.randomBoardOfMines();}
-                        GameLogic.initializeBoardOfNumbers();
-                        start = false;
-                    }
-                    if (board_of_mines[i][j]){
-                        buttons[i][j].setBackground(Color.RED);
-                        GameLogic.endGame();
-                    } else {
-                        if (board_of_numbers[i][j] == 0){
-                            buttons[i][j].setBackground(Color.GRAY);
-                        }else {
-                            buttons[i][j].setText(String.valueOf(board_of_numbers[i][j]));
-                        }
-                    }
+    private void randomBoardOfMines(){
+        Random rand = new Random();
+
+        for (int i = 0; i < liczba_min; i++){
+            int n = rand.nextInt(size_x);
+            int m = rand.nextInt(size_y);
+            if (!board_of_mines[n][m]) {
+                board_of_mines[n][m] = true;}
+            else {i--;}
+        }
+    }
+    private void howManyMines(int x, int y){
+        int suma = 0;
+        for (int k = -1; k < 2; k++) {
+            for (int q = -1; q < 2; q++) {
+                boolean test1 = k == 0 && q == 0;
+                boolean test2 = x + k < 0 || y + q < 0 || x + k == size_x || y + q == size_y;
+                if (!test1 && !test2 && board_of_mines[x + k][y + q]){
+                    suma++;
                 }
             }
         }
+        board_of_numbers[x][y] = suma;
+    }
+
+    private void initializeBoardOfNumbers(){
+        for (int i = 0; i < size_x; i++){
+            for (int j = 0; j < size_y; j++){
+                howManyMines(i, j);
+            }
+        }
+    }
+
+    private void endGame() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        String[] idxSplit = event.getSource().toString().substring(21).split(",60x54")[0].strip().split(",");
+        int x = (Integer.parseInt(idxSplit[0]) - 3) / 60;
+        int y = (Integer.parseInt(idxSplit[1]) - 2) / 54;
+        System.out.println(x + " -> " + y);
+
+        if (start){
+            randomBoardOfMines();
+            // TODO Trzeba dodać sprawdzenie czy wartość początkowa jest miną
+            initializeBoardOfNumbers();
+            start = false;
+        }
+        if (board_of_mines[x][y]){
+            buttons[x][y].setBackground(Color.RED);
+            endGame();
+        } else {
+            if (board_of_numbers[x][y] == 0){
+                buttons[x][y].setBackground(Color.GRAY);
+            }else {
+                buttons[x][y].setText(String.valueOf(board_of_numbers[x][y]));
+            }
+        }
+    }
+    public void flagPerformed(ActionEvent event) {
+        isFlagging = !isFlagging;
+        System.out.println("Flag -> " + isFlagging);
+    }
+    public static void main(String[] args){
+        GUI gui = new GUI(8, 8, 10);
     }
 }
-
